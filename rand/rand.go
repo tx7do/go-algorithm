0ef2c10359d7ce9aff79e27ace0e1c8d7bb58c21
tx7do <yanglinbo@gmail.com>
2022-02-26 11:57:41 +0800

    feat: add RandomTimestampString and RandomFloat

--- rand/rand.go
+++ rand/rand.go
@@ -5,13 +5,27 @@ import (
 	"time"
 )
 
+const layout = "2006-01-02 15:04:05"
+
 // RandomTimestamp 区间随机 - 时间戳
 func RandomTimestamp(min, max time.Time) time.Time {
 	sec := rand.Int63n(max.Unix()-min.Unix()) + min.Unix()
 	return time.Unix(sec, 0)
 }
 
+// RandomTimestampString 区间随机 - 时间戳
+func RandomTimestampString(min, max string) time.Time {
+	minT, _ := time.Parse(layout, min)
+	maxT, _ := time.Parse(layout, max)
+	return RandomTimestamp(minT, maxT)
+}
+
 // RandomInt 区间随机 - 整型
 func RandomInt(min, max int) int {
 	return rand.Intn(max-min+1) + min
+}
+
+// RandomFloat 区间随机 - 浮点型
+func RandomFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
 }
--- rand/rand_test.go
+++ rand/rand_test.go
@@ -12,10 +12,22 @@ func TestRandomInt(t *testing.T) {
 	}
 }
 
+func TestRandomFloat(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		fmt.Println(RandomFloat(1, 1000))
+	}
+}
+
 func TestRandomTimestamp(t *testing.T) {
 	minTime := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
 	maxTime := time.Date(2021, 12, 30, 0, 0, 0, 0, time.UTC)
 	for i := 0; i < 100; i++ {
-		fmt.Println(RandomTimestamp(minTime, maxTime).Format("2006-01-02 15:04:05"))
+		fmt.Println(RandomTimestamp(minTime, maxTime).Format(layout))
+	}
+}
+
+func TestRandomTimestampString(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		fmt.Println(RandomTimestampString("2021-01-01 00:00:00", "2021-12-30 00:00:00").Format(layout))
 	}
 }
